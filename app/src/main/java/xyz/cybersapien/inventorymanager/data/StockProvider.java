85b24ff451783e52a4c009dad91d278b43def22c
OGCybersapien <aditya@cybersapien.xyz>
2016-10-20 13:09:21 +0530

    Updated Stock Provider with all the methods.

--- app/src/main/java/xyz/cybersapien/inventorymanager/data/StockProvider.java
+++ app/src/main/java/xyz/cybersapien/inventorymanager/data/StockProvider.java
@@ -1,11 +1,14 @@
 package xyz.cybersapien.inventorymanager.data;
 
 import android.content.ContentProvider;
+import android.content.ContentUris;
 import android.content.ContentValues;
 import android.content.UriMatcher;
 import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.net.Uri;
 import android.support.annotation.Nullable;
+import android.util.Log;
 
 /**
  * Created by ogcybersapien on 19/10/16.
@@ -46,28 +49,188 @@ public class StockProvider extends ContentProvider {
     @Nullable
     @Override
     public Cursor query(Uri uri, String[] projection, String selection, String[] selectionArgs, String sortOrder) {
-        return null;
+        //Get readable database
+        SQLiteDatabase database = stockDbHelper.getReadableDatabase();
+
+        //Cursor to hold the data
+        Cursor cursor;
+
+        int match = stockUriMatcher.match(uri);
+        switch(match){
+
+            /**To get Items list, the match will contain value ITEMS*/
+            case ITEMS:
+                cursor = database.query(StockContract.ItemEntry.TABLE_NAME, projection, selection, selectionArgs, null,null,sortOrder);
+                break;
+            /*To get a particular Item with the specified ITEM ID*/
+            case ITEMS_ID:
+                selection = StockContract.ItemEntry._ID + "=?";
+                selectionArgs = new String[] {String.valueOf(ContentUris.parseId(uri))};
+                cursor = database.query(StockContract.ItemEntry.TABLE_NAME, projection, selection, selectionArgs, null, null, sortOrder);
+                break;
+            /*To get Suppliers list*/
+            case SUPPLIERS:
+                cursor = database.query(StockContract.SuppliersEntry.TABLE_NAME, projection, selection, selectionArgs, null,null,sortOrder);
+                break;
+            /*To get a particular Supplier with the specified SUPPLIER_ID*/
+            case SUPPLIERS_ID:
+                selection = StockContract.SuppliersEntry._ID + "=?";
+                selectionArgs = new String[] {String.valueOf(ContentUris.parseId(uri))};
+                cursor = database.query(StockContract.SuppliersEntry.TABLE_NAME, projection, selection, selectionArgs, null,null,sortOrder);
+                break;
+            default:
+                throw new IllegalArgumentException("Cannot find a match for the specified URI.");
+        }
+
+        //Set a cursor notification to notify the cursor of a change in this URIs results
+        cursor.setNotificationUri(getContext().getContentResolver(), uri);
+        return cursor;
     }
 
     @Nullable
     @Override
     public String getType(Uri uri) {
-        return null;
+
+        int match = stockUriMatcher.match(uri);
+        switch (match){
+
+            case ITEMS:
+                return StockContract.ItemEntry.CONTENT_LIST_TYPE;
+            case ITEMS_ID:
+                return StockContract.ItemEntry.CONTENT_ITEM_TYPE;
+            case SUPPLIERS:
+                return StockContract.SuppliersEntry.CONTENT_LIST_TYPE;
+            case SUPPLIERS_ID:
+                return StockContract.SuppliersEntry.CONTENT_ITEM_TYPE;
+            default:
+                throw new IllegalArgumentException("No Match found for the URI " + uri);
+        }
     }
+
 
     @Nullable
     @Override
     public Uri insert(Uri uri, ContentValues values) {
-        return null;
+        int match = stockUriMatcher.match(uri);
+        Long id;
+        switch (match){
+            case ITEMS:
+                id = addItem(values);
+                break;
+            case SUPPLIERS:
+                id = addSupplier(values);
+                break;
+            default:
+                throw new IllegalArgumentException("No table for uri " + uri);
+        }
+
+        if (id == -1){
+            Log.e(LOG_TAG, "Failed to insert row for " + uri);
+            return null;
+        }
+
+        getContext().getContentResolver().notifyChange(uri,null);
+        return ContentUris.withAppendedId(uri, id);
     }
 
     @Override
     public int delete(Uri uri, String selection, String[] selectionArgs) {
-        return 0;
+        int match = stockUriMatcher.match(uri);
+        int rowsDeleted;
+
+        SQLiteDatabase db = stockDbHelper.getWritableDatabase();
+
+        switch (match){
+            case ITEMS:
+                rowsDeleted = db.delete(StockContract.ItemEntry.TABLE_NAME, selection,selectionArgs);
+                break;
+            case ITEMS_ID:
+                selection = StockContract.ItemEntry._ID + "=?";
+                selectionArgs = new String[] {String.valueOf(ContentUris.parseId(uri))};
+                rowsDeleted = db.delete(StockContract.ItemEntry.TABLE_NAME, selection,selectionArgs);
+                break;
+            case SUPPLIERS:
+                rowsDeleted = db.delete(StockContract.SuppliersEntry.TABLE_NAME, selection,selectionArgs);
+                break;
+            case SUPPLIERS_ID:
+                selection = StockContract.SuppliersEntry._ID + "=?";
+                selectionArgs = new String[] {String.valueOf(ContentUris.parseId(uri))};
+                rowsDeleted = db.delete(StockContract.SuppliersEntry.TABLE_NAME, selection, selectionArgs);
+                break;
+            default:
+                throw new IllegalArgumentException("No match found for uri " + uri);
+        }
+        if (rowsDeleted != 0){
+            getContext().getContentResolver().notifyChange(uri,null);
+        }
+        return rowsDeleted;
     }
 
     @Override
     public int update(Uri uri, ContentValues values, String selection, String[] selectionArgs) {
-        return 0;
+
+        int match = stockUriMatcher.match(uri);
+        int rowsUpdated;
+        switch (match){
+            case ITEMS:
+                rowsUpdated =  updateItem(values,selection,selectionArgs);
+                break;
+            case ITEMS_ID:
+                selection = "=?";
+                selectionArgs = new String[] {String.valueOf(ContentUris.parseId(uri))};
+                rowsUpdated = updateItem(values, selection, selectionArgs);
+                break;
+            case SUPPLIERS:
+                rowsUpdated = updateSupplier(values,selection,selectionArgs);
+                break;
+            case SUPPLIERS_ID:
+                selection = "=?";
+                selectionArgs = new String[] {String.valueOf(ContentUris.parseId(uri))};
+                rowsUpdated = updateSupplier(values, selection, selectionArgs);
+                break;
+            default:
+                throw new IllegalArgumentException("No valid match for URI: " + uri);
+        }
+
+        if (rowsUpdated != 0){
+            getContext().getContentResolver().notifyChange(uri,null);
+        }
+
+        return rowsUpdated;
     }
+
+    private int updateSupplier(ContentValues values, String selection, String[] selectionArgs) {
+
+        if (values.size()==0){
+            return 0;
+        }
+
+        //TODO: Add Validation Logic
+
+        SQLiteDatabase db = stockDbHelper.getWritableDatabase();
+        return db.update(StockContract.SuppliersEntry.TABLE_NAME, values,selection,selectionArgs);
+    }
+
+    private int updateItem(ContentValues values, String selection, String[] selectionArgs) {
+        if (values.size() == 0){
+            return 0;
+        }
+        //TODO: Add Validation Logic
+        SQLiteDatabase db = stockDbHelper.getWritableDatabase();
+        return db.update(StockContract.ItemEntry.TABLE_NAME,values,selection,selectionArgs);
+    }
+
+    private long addItem(ContentValues values){
+        //TODO: Create Validation Logic
+        SQLiteDatabase db = stockDbHelper.getWritableDatabase();
+        return db.insert(StockContract.ItemEntry.TABLE_NAME,null,values);
+    }
+
+    private long addSupplier(ContentValues values){
+        //TODO: Create Validation Logic
+        SQLiteDatabase db = stockDbHelper.getWritableDatabase();
+        return db.insert(StockContract.SuppliersEntry.TABLE_NAME, null,values);
+    }
+
+
 }
