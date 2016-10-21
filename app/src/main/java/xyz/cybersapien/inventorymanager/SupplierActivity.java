ec714b54d750819a5e78b839806a298e3b88d9d7
OGCybersapien <aditya@cybersapien.xyz>
2016-10-21 12:12:28 +0530

    Split Main Activity in Supplier and Items Activity for lesser code complexity.

--- app/src/main/java/xyz/cybersapien/inventorymanager/SupplierActivity.java
+++ app/src/main/java/xyz/cybersapien/inventorymanager/SupplierActivity.java
@@ -1,13 +1,12 @@
 package xyz.cybersapien.inventorymanager;
 
-import android.app.LoaderManager;
 import android.content.ContentValues;
 import android.content.CursorLoader;
-import android.content.Intent;
 import android.content.Loader;
 import android.database.Cursor;
 import android.net.Uri;
 import android.support.design.widget.FloatingActionButton;
+import android.app.LoaderManager;
 import android.support.v4.app.NavUtils;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -15,16 +14,17 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.ListView;
+
 import xyz.cybersapien.inventorymanager.data.StockContract;
 
-public class MainActivity extends AppCompatActivity implements LoaderManager.LoaderCallbacks<Cursor>{
+public class SupplierActivity extends AppCompatActivity implements LoaderManager.LoaderCallbacks<Cursor>{
 
-    public static final String LOG_TAG = MainActivity.class.getName();
+    public static final String LOG_TAG = SupplierActivity.class.getName();
 
     private static final int STOCK_LOADER = 0;
 
-    //CursorAdapter for the Items and Suppliers
-    private ItemCursorAdapter customCursorAdapter;
+    //CursorAdapter for the Suppliers
+    private SupplierCursorAdapter customCursorAdapter;
 
     //ListView for the Adapter to display data
     private ListView itemsListView;
@@ -33,29 +33,30 @@ public class MainActivity extends AppCompatActivity implements LoaderManager.Loa
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.list_activity);
-        //FAB to open sales activity
+
+        //fab for creating new Supplier
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.floatingActionButton);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                //TODO: Implement onClick method.
+                //TODO: Implement add new supplier intent
             }
         });
 
-        setTitle("Items in Stock");
-        //get the empty ListView
+        setTitle("Suppliers");
         itemsListView = (ListView) findViewById(R.id.main_list);
-        customCursorAdapter = new ItemCursorAdapter(this, null);
+        customCursorAdapter = new SupplierCursorAdapter(this, null);
         itemsListView.setAdapter(customCursorAdapter);
 
-        //Start the Loader
+        //start the Loader
         getLoaderManager().initLoader(STOCK_LOADER, null, this);
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.main_menu, menu);
-        menu.findItem(R.id.action_menu_list_toggle).setTitle("Supplier List");
+        MenuItem changeActivityMenuOption = menu.findItem(R.id.action_menu_list_toggle);
+        changeActivityMenuOption.setVisible(false);
         return true;
     }
 
@@ -63,36 +64,34 @@ public class MainActivity extends AppCompatActivity implements LoaderManager.Loa
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()){
             case R.id.action_data_insert_dummy_data:
-                insertItems();
+                insertSuppliers();
                 break;
-            case R.id.action_menu_list_toggle:
-                Intent goToSupplier = new Intent(this,SupplierActivity.class);
-                startActivity(goToSupplier);
+            case android.R.id.home:
+                NavUtils.navigateUpFromSameTask(SupplierActivity.this);
                 break;
         }
         return true;
     }
 
-    private void insertItems(){
+    private void insertSuppliers(){
         ContentValues values = new ContentValues();
-        values.put(StockContract.ItemEntry.COLUMN_ITEM_NAME, "Item");
-        values.put(StockContract.ItemEntry.COLUMN_ITEM_PRICE, 25.5);
-        values.put(StockContract.ItemEntry.COLUMN_ITEM_SUPPLIER_ID, 2);
-        Uri itemsUri = StockContract.ItemEntry.ITEMS_CONTENT_URI;
-        getContentResolver().insert(itemsUri, values);
+        values.put(StockContract.SuppliersEntry.COLUMN_SUPPLIER_NAME, "Charlie Harper");
+        values.put(StockContract.SuppliersEntry.COLUMN_SUPPLIER_PHONE, "7696497298");
+        values.put(StockContract.SuppliersEntry.COLUMN_SUPPLIER_EMAIL, "aditya@cybersapien.xyz");
+        Uri suppliersUri = StockContract.SuppliersEntry.SUPPLIERS_CONTENT_URI;
+        getContentResolver().insert(suppliersUri, values);
     }
 
     @Override
     public Loader<Cursor> onCreateLoader(int id, Bundle args) {
         String[] projection = {
-                StockContract.ItemEntry._ID,
-                StockContract.ItemEntry.COLUMN_ITEM_NAME,
-                StockContract.ItemEntry.COLUMN_ITEM_QUANTITY,
-                StockContract.ItemEntry.COLUMN_ITEM_PRICE
+                StockContract.SuppliersEntry._ID,
+                StockContract.SuppliersEntry.COLUMN_SUPPLIER_NAME,
+                StockContract.SuppliersEntry.COLUMN_SUPPLIER_PHONE,
+                StockContract.SuppliersEntry.COLUMN_SUPPLIER_EMAIL
         };
-
-        return new CursorLoader(this, StockContract.ItemEntry.ITEMS_CONTENT_URI,
-                projection, null, null, null);
+        return new CursorLoader(this, StockContract.SuppliersEntry.SUPPLIERS_CONTENT_URI,
+                projection, null,null,null);
     }
 
     @Override
@@ -104,6 +103,4 @@ public class MainActivity extends AppCompatActivity implements LoaderManager.Loa
     public void onLoaderReset(Loader<Cursor> loader) {
         customCursorAdapter.swapCursor(null);
     }
-
-
 }
